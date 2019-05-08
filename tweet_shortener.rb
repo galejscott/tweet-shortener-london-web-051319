4bb2fbf24b43e238bc9a8af234ad3c18f4d2a38d
Scott Gale <galejscott@gmail.com>
2019-05-08 22:21:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ def dictionary
 end
 
 def word_substituter(string)
-  string.split.collect do |string|
+  string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
