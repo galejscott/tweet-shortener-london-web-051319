38e85848b14ef10adb46b1a5d7d25577b93a69dd
Scott Gale <galejscott@gmail.com>
2019-05-08 21:42:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,10 +25,10 @@ def word_substituter(string)
 end
 
 def bulk_tweet_shortener(array)
-  array.each do |tweets|
-    puts word_substituter(tweets)
+  array.each do |tweet|
+    puts word_substituter(tweet)
   end
 end
 
-def selective_tweet_shortener(tweets)
-  if 
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
