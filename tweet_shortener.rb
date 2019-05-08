4a3d2886f3d3e87fa9e4af1a7051426457424e6b
Scott Gale <galejscott@gmail.com>
2019-05-08 22:23:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,3 +45,11 @@ def shortened_tweet_truncator(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140 
+    word_substituter(tweet)[0..140] + '...'
+  else 
+    tweet 
+  end 
+end 
